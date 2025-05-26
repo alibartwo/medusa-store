cbe6d13ea1f33cf8b163146ad74b231bfa247d55
alibartwo <ali@alibartu.com>
2025-05-26 12:30:13 +0300

    chore: start from scratch

--- medusa/src/modules/cms/index.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@medusajs/framework/utils";
-import CmsModuleService from "./service";
-
-export const CMS_MODULE = "cms";
-
-export default Module(CMS_MODULE, {
-  service: CmsModuleService,
-});
