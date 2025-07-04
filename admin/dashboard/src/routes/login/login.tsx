5355df0ebed171a614210fb3a2e0a5367edf71f3
alibartwo <ali@alibartu.com>
2025-07-04 15:01:31 +0300

    chore: comment avatarbox importation

--- admin/dashboard/src/routes/login/login.tsx
+++ admin/dashboard/src/routes/login/login.tsx
@@ -6,7 +6,7 @@ import { Link, useLocation, useNavigate } from "react-router-dom"
 import * as z from "zod"
 
 import { Form } from "../../components/common/form"
-import AvatarBox from "../../components/common/logo-box/avatar-box"
+//import AvatarBox from "../../components/common/logo-box/avatar-box"
 import { useSignInWithEmailPass } from "../../hooks/api"
 import { isFetchError } from "../../lib/is-fetch-error"
 import { useExtension } from "../../providers/extension-provider"
