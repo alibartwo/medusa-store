3a4718dc61b366483880cd2d3088737c238ca8ea
alibartwo <ali@alibartu.com>
2025-07-16 13:28:27 +0000

    feat: add file module

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -5,7 +5,6 @@ loadEnv(process.env.NODE_ENV || "development", process.cwd());
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
-    redisUrl: process.env.REDIS_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
@@ -26,6 +25,21 @@ module.exports = defineConfig({
       options: {
         apiKey: process.env.CMS_API_KEY,
       },
+    },
+    {
+      resolve: "@medusajs/medusa/file",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/file-local",
+            id: "local",
+            options: {
+              upload_dir: "static",
+              backend_url: process.env.MEDUSA_BACKEND_URL + "/static",
+            },
+          },
+        ],
+      }, 
     },
     {
       resolve: "@medusajs/medusa/payment",
