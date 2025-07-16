58079a6a76b180b4ee0f66c480aa34a4781919e6
alibartwo <ali@alibartu.com>
2025-07-16 16:25:51 +0300

    chore: disable redis for now

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -5,7 +5,6 @@ loadEnv(process.env.NODE_ENV || "development", process.cwd());
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
-    redisUrl: process.env.REDIS_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
@@ -14,6 +13,7 @@ module.exports = defineConfig({
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
   },
+
   admin: {
     disable: true,
   },
